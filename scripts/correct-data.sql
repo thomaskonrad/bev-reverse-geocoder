30a9828dbaea604d4b01e539da516046e77393e5
Thomas Konrad <mail@thomaskonrad.at>
2017-05-26 08:47:43 +0200

    Improved data correction (street vs. place).

--- scripts/correct-data.sql
+++ scripts/correct-data.sql
@@ -4,11 +4,26 @@ UPDATE bev_addresses SET address_type='place'  WHERE street IN (SELECT DISTINCT(
 -- Correct addresses which are marked as "street" type but are of type "place".
 UPDATE bev_addresses SET address_type = 'street'
 --SELECT municipality, street, count(house_number) FROM bev_addresses
-WHERE address_type = 'unknown'
-  AND
+WHERE
   (
 	  -- Municipalities that don't have any localities but only street names.
-		municipality IN ('Wien', 'Dornbirn', 'Eisenstadt', 'Gerasdorf bei Wien', 'Fraxern', 'Höchst', 'Horn', 'Innsbruck', 'Klagenfurt am Wörthersee', 'Lustenau', 'Kapfenberg', 'Karlstein an der Thaya', 'Klaus', 'Klosterneuburg', 'Koblach', 'Korneuburg', 'Landeck', 'Langen bei Bregenz', 'Langenrohr', 'Leithaprodersdorf', 'Lienz', 'Liezen', 'Linz', 'Ludesch', 'Neulengbach', 'Neusiedl am See', 'Oberhofen im Inntal', 'Passail', 'Peggau', 'Pinkafeld', 'Pitten', 'Podersdorf am See', 'Poggersdorf', 'Polling in Tirol', 'Pottendorf', 'Puchenau', 'Purbach am Neusiedler See', 'Purkersdorf', 'Pörtschach am Wörther See', 'Rankweil', 'Ried im Innkreis', 'Rust', 'Röthis', 'Salzburg', 'Sautens', 'Schladming', 'Schlins', 'Schärding', 'St. Pölten', 'Stockerau', 'Straßwalchen', 'Teesdorf', 'Traunkirchen', 'Trieben', 'Tulln an der Donau', 'Uderns', 'Umhausen', 'Unterach am Attersee', 'Uttendorf', 'Baden', 'Vöcklabruck', 'Villach', 'Vils', 'Wagna', 'Wildschönau', 'Winzendorf-Muthmannsdorf', 'Wolfsegg am Hausruck', 'Wolfurt', 'Wolkersdorf im Weinviertel', 'Wulkaprodersdorf', 'Wöllersdorf-Steinabrückl', 'Wörgl', 'Würmla', 'Ybbs an der Donau', 'Ybbsitz', 'Zams', 'Zeiselmauer-Wolfpassing', 'Zelking-Matzleinsdorf', 'Zell am Moos', 'Zell am See', 'Zell am Ziller', 'Zell an der Pram', 'Zeltweg', 'Zillingdorf', 'Zistersdorf', 'Zurndorf', 'Zwentendorf an der Donau', 'Zwettl an der Rodl', 'Zwischenwasser', 'Zwölfaxing', 'Öblarn', 'Übelbach', 'Übersaxen')
+		municipality IN (
+		  'Wien', 'Dornbirn', 'Eisenstadt', 'Gerasdorf bei Wien', 'Fraxern', 'Höchst', 'Horn', 'Innsbruck',
+		  'Klagenfurt am Wörthersee', 'Lustenau', 'Kapfenberg', 'Karlstein an der Thaya', 'Klaus', 'Klosterneuburg',
+		  'Koblach', 'Korneuburg', 'Landeck', 'Langen bei Bregenz', 'Langenrohr', 'Leithaprodersdorf', 'Lienz', 'Liezen',
+		  'Linz', 'Ludesch', 'Neulengbach', 'Neusiedl am See', 'Oberhofen im Inntal', 'Passail', 'Peggau', 'Pinkafeld',
+		  'Pitten', 'Podersdorf am See', 'Poggersdorf', 'Polling in Tirol', 'Pottendorf', 'Puchenau',
+		  'Purbach am Neusiedler See', 'Purkersdorf', 'Pörtschach am Wörther See', 'Rankweil', 'Ried im Innkreis', 'Rust',
+		  'Röthis', 'Salzburg', 'Sautens', 'Schladming', 'Schlins', 'Schärding', 'St. Pölten', 'Stockerau', 'Straßwalchen',
+		  'Teesdorf', 'Traunkirchen', 'Trieben', 'Tulln an der Donau', 'Uderns', 'Umhausen', 'Unterach am Attersee',
+		  'Uttendorf', 'Baden', 'Vöcklabruck', 'Villach', 'Vils', 'Wagna', 'Wildschönau', 'Winzendorf-Muthmannsdorf',
+		  'Wolfsegg am Hausruck', 'Wolfurt', 'Wolkersdorf im Weinviertel', 'Wulkaprodersdorf', 'Wöllersdorf-Steinabrückl',
+		  'Wörgl', 'Würmla', 'Ybbs an der Donau', 'Ybbsitz', 'Zams', 'Zeiselmauer-Wolfpassing', 'Zelking-Matzleinsdorf',
+		  'Zell am Moos', 'Zell am See', 'Zell am Ziller', 'Zell an der Pram', 'Zeltweg', 'Zillingdorf', 'Zistersdorf',
+		  'Zurndorf', 'Zwentendorf an der Donau', 'Zwettl an der Rodl', 'Zwischenwasser', 'Zwölfaxing', 'Öblarn',
+		  'Übelbach', 'Übersaxen', 'Wals-Siezenheim', 'Weiden am See', 'Weiler', 'Weißenbach am Lech',
+		  'Weißenkirchen in der Wachau', 'Wels', 'Wiener Neudorf', 'Wiener Neustadt', 'Wieselburg', 'Winden am See'
+		)
  
     -- Common patterns for street names in Austria.
     OR LOWER(street) LIKE '%straße'
@@ -138,16 +153,39 @@ WHERE address_type = 'unknown'
 		-- Special street names that are not recognizable as streets as such just by their name.
 		-- Please submit a pull request if you know more examples!
 		-- This is especially common in Vorarlberg (see Andelsbuch, for example).
-		OR street IN ('Bofel', 'Buxera', 'Gugger Nussbaum', 'Schufla', 'Bola', 'Breite', 'Alberau', 'Gehren', 'Grünegger', 'Binsenfeld', 'Blumenau', 'Auf Litschis', 'Polder', 'Bungat', 'Riedle', 'Herrenfeld', 'Riedgarten', 'Liebera', 'Mühlwasen', 'Garazerfeld', 'Hasenau', 'Gässli', 'Neugebäude', 'Melans', 'Ranser Feld', 'Winkelgarten', 'Winklfeld', 'Itter', 'Grund', 'Grunholz', 'Wirth', 'Ach', 'Ruhmanen', 'Gaß', 'Scheidbuchen', 'Fahl', 'Buchen', 'Hüngen', 'Loch', 'Heidegg', 'Bauern', 'Amtsschmiedhöhe', 'Föhrenwald', 'Frauental', 'Fröschlpoint', 'Marienpark', 'Graben Amstetten', 'Graben Ulmerfeld', 'Platte', 'Daliebis', 'Bazol', 'Haslat', 'Schüttenacker', 'Graf-Rudolf-Wuhrgang', 'Graf-Hugo-Wuhrgang', 'Churer Tor', 'Gempala', 'Motta', 'Gosta', 'Troja', 'Untere Gosta', 'Obere Gosta', 'Kilknerwald', 'Bonawinkel', 'Inner Tobel', 'Sprisaloch', 'Zerfall', 'Boda', 'Güatli', 'Floßländ', 'Quellenhof', 'Hoher Markt', 'Walchsee', 'Walchsee', 'Waldkirchen an der Thaya', 'Wals-Siezenheim', 'Wattens', 'Weibern', 'Weiden am See', 'Weiler', 'Weißenbach am Lech', 'Weißenkirchen in der Wachau', 'Wels', 'Wiener Neudorf', 'Wiener Neustadt', 'Wieselburg', 'Wildendürnbach', 'Ringmauer', 'Conrad-Lester-Hof', 'Winden am See', 'Schmollfeld', 'Großmühlhäuser', 'Lötz')
+		OR street IN (
+		  'Bofel', 'Buxera', 'Gugger Nussbaum', 'Schufla', 'Bola', 'Breite', 'Alberau', 'Gehren', 'Grünegger', 'Binsenfeld',
+		  'Blumenau', 'Auf Litschis', 'Polder', 'Bungat', 'Riedle', 'Herrenfeld', 'Riedgarten', 'Liebera', 'Mühlwasen',
+		  'Garazerfeld', 'Hasenau', 'Gässli', 'Neugebäude', 'Melans', 'Ranser Feld', 'Winkelgarten', 'Winklfeld', 'Itter',
+		  'Grund', 'Grunholz', 'Wirth', 'Ach', 'Ruhmanen', 'Gaß', 'Scheidbuchen', 'Fahl', 'Buchen', 'Hüngen', 'Loch',
+		  'Heidegg', 'Bauern', 'Amtsschmiedhöhe', 'Föhrenwald', 'Frauental', 'Fröschlpoint', 'Marienpark',
+		  'Graben Amstetten', 'Graben Ulmerfeld', 'Platte', 'Daliebis', 'Bazol', 'Haslat', 'Schüttenacker',
+		  'Graf-Rudolf-Wuhrgang', 'Graf-Hugo-Wuhrgang', 'Churer Tor', 'Gempala', 'Motta', 'Gosta', 'Troja', 'Untere Gosta',
+		  'Obere Gosta', 'Kilknerwald', 'Bonawinkel', 'Inner Tobel', 'Sprisaloch', 'Zerfall', 'Boda', 'Güatli', 'Floßländ',
+		  'Quellenhof', 'Hoher Markt', 'Walchsee', 'Walchsee', 'Ringmauer', 'Conrad-Lester-Hof', 'Großmühlhäuser', 'Lötz'
+		)
 	)
 	AND
 	(
     -- Places that end with "ring" but are not streets for sure.
     street NOT IN (
-      'Badhöring', 'Biedring', 'Daring', 'Dobring', 'Ebring', 'Ehring', 'Engljähring', 'Euring', 'Evangelischer Friedhof Simmering', 'Feistring', 'Felbring', 'Feuerhalle Simmering', 'Feuring', 'Fischering', 'Fischhamering', 'Födering', 'Gadering', 'Gattring', 'Geigering', 'Gemering', 'Giering', 'Gimpering', 'Ginshöring', 'Gunsering', 'Guttaring', 'Gössering', 'Habring', 'Hallenbad Simmering', 'Haudering', 'hiering', 'Hilkering', 'Hintering', 'Hintring', 'Höring', 'Ingering II', 'Innernöring', 'Jauring', 'Kaffring', 'Kellnering', 'Kleinsemmering', 'Kring', 'Köfering', 'Kühnring', 'Liedering', 'Littring', 'Mahring', 'Mairing', 'Marchtring', 'Marchtring', 'Mittergafring', 'Mähring', 'Mödring', 'Mühring', 'Niederbairing', 'Oberbairing', 'Obergafring', 'Oberjahring', 'Oberkansering', 'Oberring', 'Oberrühring', 'Oberschöfring', 'Ochsenharing', 'Olsaring', 'Pehring', 'Pengering', 'Pesenlittring', 'Pöbring', 'Pühring', 'Rackering', 'Ragering', 'Reichering', 'Reitering', 'Rubring', 'Rühring', 'Schillering', 'Schnittering', 'Schoppering', 'Seiring', 'Semering', 'Semmering', 'Seyring', 'Silbering', 'Sindhöring', 'Stamering', 'Steiring', 'Unering', 'Unterkansering', 'Unterpassering', 'Uring', 'Vordernöring', 'Wahring', 'Waidring', 'Walkering', 'Wassering', 'Wienering', 'Wolfring', 'Wöbring', 'Wögring', 'Wölzing-Fischering', 'Zintring', 'Zwaring', 'Zwischensimmering', 'Zöbring',
+      'Badhöring', 'Biedring', 'Daring', 'Dobring', 'Ebring', 'Ehring', 'Engljähring', 'Euring',
+      'Evangelischer Friedhof Simmering', 'Feistring', 'Felbring', 'Feuerhalle Simmering', 'Feuring', 'Fischering',
+      'Fischhamering', 'Födering', 'Gadering', 'Gattring', 'Geigering', 'Gemering', 'Giering', 'Gimpering',
+      'Ginshöring', 'Gunsering', 'Guttaring', 'Gössering', 'Habring', 'Hallenbad Simmering', 'Haudering', 'hiering',
+      'Hilkering', 'Hintering', 'Hintring', 'Höring', 'Ingering II', 'Innernöring', 'Jauring', 'Kaffring', 'Kellnering',
+      'Kleinsemmering', 'Kring', 'Köfering', 'Kühnring', 'Liedering', 'Littring', 'Mahring', 'Mairing', 'Marchtring',
+      'Marchtring', 'Mittergafring', 'Mähring', 'Mödring', 'Mühring', 'Niederbairing', 'Oberbairing', 'Obergafring',
+      'Oberjahring', 'Oberkansering', 'Oberring', 'Oberrühring', 'Oberschöfring', 'Ochsenharing', 'Olsaring', 'Pehring',
+      'Pengering', 'Pesenlittring', 'Pöbring', 'Pühring', 'Rackering', 'Ragering', 'Reichering', 'Reitering', 'Rubring',
+      'Rühring', 'Schillering', 'Schnittering', 'Schoppering', 'Seiring', 'Semering', 'Semmering', 'Seyring',
+      'Silbering', 'Sindhöring', 'Stamering', 'Steiring', 'Unering', 'Unterkansering', 'Unterpassering', 'Uring',
+      'Vordernöring', 'Wahring', 'Waidring', 'Walkering', 'Wassering', 'Wienering', 'Wolfring', 'Wöbring', 'Wögring',
+      'Wölzing-Fischering', 'Zintring', 'Zwaring', 'Zwischensimmering', 'Zöbring',
 
       -- Places that end with "rain" but are not streets for sure.
-      'Halbenrain', 'Achrain', 'Draurein', 'Harrain', 'Hochrain', 'Hungerrain', 'Hungerrain', 'Niedermauern-Rain', 'Podrain', 'Rotrain', 'Sellrain', 'Silberrain', 'Unterkrain', 'Wagrain',
+      'Halbenrain', 'Achrain', 'Draurein', 'Harrain', 'Hochrain', 'Hungerrain', 'Hungerrain', 'Niedermauern-Rain',
+      'Podrain', 'Rotrain', 'Sellrain', 'Silberrain', 'Unterkrain', 'Wagrain',
 
       -- Places that end with "ried" but are not streets for sure.
       'Ritzenried',
@@ -190,6 +228,7 @@ UPDATE bev_addresses SET house_number=split_part(house_number, ' ,alle', 1)
 UPDATE bev_addresses SET street = 'Bahnstraße' WHERE municipality = 'Ebreichsdorf' AND street LIKE 'Bahnstraße %';
 
 
-
--- TODO: Wildschönau. There are several localities which have themselves street names. They are depicted as "Street, locality". They need to be separated.
--- TODO: Wolkersdorf im Weinviertel.
+-- In Wildschönau, streets are depicted as "street, locality". We delete the part after the comma because the locality
+-- can be found in the "locality" column anyway.
+UPDATE bev_addresses SET street=split_part(street, ', ', 1)
+  WHERE municipality = 'Wildschönau';
